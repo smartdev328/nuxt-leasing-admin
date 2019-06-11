a96281747cea6c01227f32d4740371767bce7276
Tarj Pecco <smartdev@smartdevs-iMac.local>
2019-06-11 16:26:04 -0500

    Complete pagination and listing products

---
+++ components/Loading.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="loading">
+    <img src="~/assets/images/preloader.gif" />
+  </div>
+</template>
+
+<style>
+.loading {
+  float: left;
+  width: 100%;
+  height: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.loading img {
+  width: 100px;
+  height: 100px;
+}
+</style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,17 +2,85 @@
   <div class="products">
     <b-row>
       <b-col lg="12">
-        <c-table striped caption="List of All Products"></c-table>
+        <b-card :header="header">
+          <b-table
+            :striped="striped"
+            responsive="sm"
+            :items="products"
+            :fields="fields"
+          >
+          </b-table>
+          <nav>
+            <b-pagination
+              :total-rows="totalRows"
+              :per-page="perPage"
+              v-model="currentPage"
+              prev-text="Prev"
+              next-text="Next"
+              @change="getProducts"
+              hide-goto-end-buttons
+            />
+          </nav>
+        </b-card>
+        <Loading v-show="loading" />
       </b-col>
     </b-row>
   </div>
 </template>
 
 <script>
-  import cTable from '../base/table.vue'
+import axios from 'axios'
 
-  export default {
-    name: 'tables',
-    components: {cTable}
+import Loading from '~/components/Loading.vue'
+
+export default {
+  name: 'products',
+  components: {
+    Loading,
+  },
+  data: () => {
+    return {
+      products: [],
+      fields: [
+        {key: 'id', sortable: true, label: 'ID'},
+        {key: 'brand', sortable: true },
+        {key: 'model', sortable: true },
+        {key: 'oVariant', sortable: true, label: 'oVariant' },
+        {key: 'variant', sortable: true },
+        {key: 'year', sortable: true},
+        {key: 'acquisitionCost', sortable: true },
+        {key: 'scrapValues'},
+        {key: 'leasingPeriods'}
+      ],
+      currentPage: 1,
+      perPage: 3,
+      totalRows: 0,
+      header: "List of All Products",
+      loading: false,
+      striped: true,
+    }
+  },
+  mounted() {
+    this.getProducts(1)
+  },
+  methods: {
+    getProducts(pageNum) {
+      this.currentPage = pageNum
+      this.loading = true
+      axios.get('/api/v1/products/', {
+        params: {
+          limit: this.perPage,
+          offset: (this.currentPage - 1) * this.perPage + 0,
+        }
+      }).then(response => {
+        this.products = response.data.results
+        this.totalRows = response.data.total
+        this.loading = false
+        const startIndex = (this.currentPage - 1) * this.perPage + 1
+        const endIndex = this.currentPage * this.perPage > this.totalRows ? this.totalRows : this.currentPage * this.perPage
+        this.header = `List of All Products (${startIndex} - ${endIndex} / ${this.totalRows})`
+      })
+    }
   }
+}
 </script>
