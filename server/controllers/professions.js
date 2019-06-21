6db7c7ea898bf6d962a49b5f48708c5d468251e5
Tarj Pecco <smartdev@smartdevs-iMac.local>
2019-06-21 04:51:10 -0500

    Create professions CRUD apis

--- server/controllers/professions.js
+++ server/controllers/professions.js
@@ -1,10 +1,25 @@
 const _ = require('lodash')
 const logger = require('../lib/logger')
+const exceptions = require('../lib/exceptions')
 const professions = require('../service').professions
 const SequelizeEmptyResultError = require('../models').Sequelize
   .EmptyResultError
 
 module.exports = {
+  createProfession: (req, res) => {
+    return professions.create(req.body)
+      .then(newProfession => professions.fullRes(newProfession))
+      .then(output =>
+        res.status(201).send(simpleRes('Successfully Created Profession', output))
+      )
+      .catch(exceptions.InvalidInputError, err =>
+        res.status(400).send(invalidInputRes(0, err.errors))
+      )
+      .catch(err => {
+        logger.error(err)
+        return res.status(500).send()
+      })
+  },
   listProfessions: (req, res) => {
     const limit = req.swagger.params.limit.value
     const offset = req.swagger.params.offset.value
@@ -39,6 +54,61 @@ module.exports = {
         logger.error(err)
         return res.status(500).send(err)
       })
+  },
+  getProfession: (req, res) => {
+    logger.info('Getting a profession')
+    const id = req.swagger.params.professionId.value
+    return professions
+      .get(id, { rejectOnEmpty: true })
+      .then(profession => professions.fullRes(profession))
+      .then(data =>
+        res.status(200).send(simpleRes('Successfully Retrieved Profession', data))
+      )
+      .catch(SequelizeEmptyResultError, () => res.status(404).send())
+      .catch(err => {
+        logger.error(err)
+        res.status(500).send()
+      })
+  },
+  modifyProfession: (req, res) => {
+    logger.info('Modifying a profession')
+    const id = req.swagger.params.professionId.value
+
+    return professions.modify(id, req.body)
+      .then(prod => professions.fullRes(prod))
+      .then(data =>
+        res.status(201).send(simpleRes('Successfully Modified Profession', data))
+      )
+      .catch(exceptions.InvalidInputError, err =>
+        res.status(400).send(invalidInputRes(0, err.errors))
+      )
+      .catch(SequelizeEmptyResultError, () => res.status(404).send())
+      .catch(err => {
+        logger.error(err)
+        return res.status(500).send()
+      })
+  },
+  deleteProfession: (req, res) => {
+    logger.info('Deleting a profession')
+    const professionId = req.swagger.params.professionId.value
+    let response = {}
+    return professions
+      .get(professionId, { rejectOnEmpty: true })
+      .then(profession => professions.fullRes(profession))
+      .then(data => {
+        response = data
+        return professions.delete(professionId)
+      })
+      .then(() =>
+        res
+          .status(200)
+          .send(simpleRes('Successfully Deleted Profession', response))
+      )
+      .catch(SequelizeEmptyResultError, () => res.status(404).send())
+      .catch(err => {
+        logger.error(err)
+        res.status(500).send()
+      })
   }
 }
 
@@ -48,4 +118,16 @@ const searchRes = (datas, total, offset, limit) => ({
   offset: offset,
   limit: limit,
   results: datas
+})
+
+const invalidInputRes = (code, err) => ({
+  status: 400,
+  code: code,
+  message: 'Invalid Input(s)',
+  errors: err
+})
+
+const simpleRes = (message, data) => ({
+  message: message,
+  data: data
 })
--- server/service/professions/professions.js
+++ server/service/professions/professions.js
@@ -4,6 +4,15 @@ const Promise = require('bluebird')
 const Professions = require('../../models').Profession
 
 module.exports = {
+  create: params => {
+    return Professions.create({
+      name: params.name,
+      price: params.price,
+      icon: params.icon,
+      link: params.link,
+      shortDescription: params.shortDescription
+    })
+  },
   search: options => {
     const where = {}
     const include = []
@@ -19,6 +28,38 @@ module.exports = {
         limit: options.limit,
         offset: options.offset
       })
+    })
+  },
+  modify: (equipmentId, params) => {
+    const id = equipmentId
+    return Professions.findOne({
+      where: { id: id },
+      rejectOnEmpty: true
+    })
+      .then(equipment =>
+        Promise.props({
+          equipmentUpdate: equipment.update({
+            name: params.name,
+            price: params.price,
+            icon: params.icon,
+            link: params.link,
+            shortDescription: params.shortDescription
+          })
+        })
+      )
+      .then(result => result.equipmentUpdate)
+  },
+  get: (equipmentId, options = {}) => {
+    const id = equipmentId
+    return Professions.findOne({
+      where: { id: id },
+      rejectOnEmpty: options.rejectOnEmpty ? options.rejectOnEmpty : false
+    })
+  },
+  delete: equipmentId => {
+    return Professions.destroy({
+      where: { id: equipmentId },
+      rejectOnEmpty: true
     })
   },
   fullRes: profession => {
